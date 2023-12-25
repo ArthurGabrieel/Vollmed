b13f8fab0949b041733db2a190b667f85cd8fc90
Arthur Gabriel Lima Gomes <78980894+ArthurGabrieel@users.noreply.github.com>
2023-12-25 16:12:10 -0300

    First commit

---
+++ Vollmed/Services/WebService.swift
@@ -0,0 +1,17 @@
+//
+//  WebService.swift
+//  Vollmed
+//
+//  Created by Arthur Gabriel on 25/12/2023.
+//
+
+import Foundation
+
+struct WebService {
+    
+    private let baseURL = "http://localhost:3000"
+    
+    func getAllSpecialists() {
+        
+    }
+}
---
+++ Vollmed/VollmedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VollmedApp.swift
+//  Vollmed
+//
+//  Created by Arthur Gabriel on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct VollmedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
